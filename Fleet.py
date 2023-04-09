e4058e38d2763cbfac611d31d2b5a78c6a07c791
KCable <126310715+KCable@users.noreply.github.com>
2023-04-08 19:22:30 -0700

    I created two new classes. Fleet and Herd are collections of robots and dinos that will be randomly chosen durning the game.

---
+++ Fleet.py
@@ -0,0 +1,7 @@
+from Robot import Robot
+
+class Fleet:
+    def __init__(self):
+        self.robot_list = [Robot("R2-D2"), Robot("Bumblebee"), Robot("Marvin")]
+        pass
+    
---
+++ Herd.py
@@ -0,0 +1,6 @@
+from Dinosaur import Dinosaur
+
+class Herd:
+    def __init__(self):
+        self.dino_list = [Dinosaur("Rex"), Dinosaur("Godzilla"), Dinosaur("Barney")]
+        pass
